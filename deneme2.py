412fe307db5e8c8531f35b79d4912c7d4a4b3752
Kasim Sert <kasimsert@gmail.com>
2014-06-29 17:48:21 +0300

    deneme

---
+++ deneme2.py
@@ -0,0 +1,24 @@
+def a():
+    sentence = "the quick brown fox jumps over the lazy dog"
+    words = sentence.split()
+    word_lengths = []
+    for word in words:
+        if word != "the":
+            word_lengths.append(len(word))
+
+def b():
+    sentence = "the quick brown fox jumps over the lazy dog"
+    words=sentence.split()
+    words_length= [len(word) for word in words if word != 'the']
+    return words_length
+
+def c():
+    nums=[10,11,-11,-22,-33]
+    numsp = [num for num in nums if num > 0 ]
+    return numsp
+
+import sys
+try:
+    raise Exception("kasim")
+except Exception as  errstr:
+    sys.stderr.write(str(errstr))
