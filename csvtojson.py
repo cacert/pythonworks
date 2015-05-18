4d00d114aa7be9e28d48a3a40437f284259a6126
Kasim Sert <kasimsert@gmail.com>
2015-05-18 23:30:37 +0300

    adaptation to common format

--- csvtojson.py
+++ csvtojson.py
@@ -4,45 +4,63 @@ Created on May 5, 2015
 @author: kasimsert
 '''
 import csv, json
+from web.db import database
 
 database = []
 
-def csvtojson():
-    def search(name):
-        for p in database:
-            if p['hostName'] == name:
-                return p
+def search(name):
+    for p in database:
+        if p['HOSTNAME'] == name:
+            return p
 
-    with open('stat.csv', 'rb') as csvfile:
-        csvfields = ('hostName','serviceName','callcount','successcount','rt')
+def csvtojson():
+    with open('/Users/kasimsert/Desktop/tez/data/data-01.csv', 'rb') as csvfile:
+        csvfields = ('SERVICENAME','COUNT','AVGDURATION','HOSTOID','HOSTNAME')
         reader = csv.DictReader(csvfile, fieldnames=csvfields)
         group = None
         index = 0
         for row in reader:
-            group = row['hostName']
+            group = row['HOSTNAME']
             a = search(group)
             if( not a):
                 a={}
-                a['hostName']=row['hostName']
+                a['HOSTNAME']=row['HOSTNAME']
                 index=index+1
                 a['index']=index
                 a['children']=[]
-                database.append(a)
-            a['children'].append({ "rt": row['rt'].rstrip(),"successcount": row['successcount'].rstrip(),
-                                    'callcount': row['callcount'].rstrip(), 'serviceName': row['serviceName'].rstrip()})
+                database.append(a) 
+            a['children'].append({ "AVGDURATION": row['AVGDURATION'].rstrip(),"COUNT": row['COUNT'].rstrip(),
+                                    'COUNT': row['COUNT'].rstrip(), 'SERVICENAME': row['SERVICENAME'].rstrip()})   
+         
+    with open('/Users/kasimsert/Desktop/tez/data/out-01.json', 'w') as outfile:
+        json.dump(database, outfile)
 
-
-
-def appendDate(columname, value):
-    with open('stat_son.csv', 'w') as outfile:
-        with open('stat.csv', 'rb') as csvfile:
+def appendDate(filename,columname, value):
+    with open(filename+'son', 'w') as outfile:
+        with open(filename, 'rb') as csvfile:
             i =0
             for line in csvfile:
                 if(i==0):
                     outfile.write(line.rstrip()+',' +columname+'\n')
                 else:
                     outfile.write(line.rstrip()+',' +value+'\n')
+                i=i+1                
+def mergeFiles(files):
+    with open('stats.csv', 'w') as outfile:
+        for file in files:
+            i = 0
+            with open(file+'son', 'rb') as infile:
+                for line in infile:
+                    if(i==0):
+                        pass
+                    else:
+                        outfile.write(line.rstrip()+'\n')
+                    i=i+1
+                
 
-appendDate('date','2015/05/21')
-
-print 'atom is cool'
+# appendDate('/Users/kasimsert/Desktop/tez/data/data-01.csv','date','2015/05/13')
+# appendDate('/Users/kasimsert/Desktop/tez/data/data-02.csv','date','2015/05/12')
+# appendDate('/Users/kasimsert/Desktop/tez/data/data-03.csv','date','2015/05/11')
+# csvtojson()
+mergeFiles(['/Users/kasimsert/Desktop/tez/data/data-01.csv','/Users/kasimsert/Desktop/tez/data/data-02.csv','/Users/kasimsert/Desktop/tez/data/data-03.csv'])
+    
